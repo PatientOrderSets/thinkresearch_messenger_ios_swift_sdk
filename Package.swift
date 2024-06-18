c22843a023a232728f674adc50ee79ff78979015
naseem-dev <naseem@prismcloudinc.com>
2024-06-19 01:24:54 +0530

    Version Update

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
             targets: ["thinkresearch_messenger_ios_swift_sdk"]
         ),
     ],
+    
     targets: [
         .target(
             name: "thinkresearch_messenger_ios_swift_sdk",
@@ -25,29 +26,29 @@ let package = Package(
         ),
              .binaryTarget(
                    name: "App",
-                   url: "https://github.com/naseem123/chatbot_sdk_channel/releases/download/1.2.9%2B6/App.xcframework.zip",
-                   checksum: "82b89e0d34c594bcdfb75104f59bebf8e06348f0a90f93356d56be475357a569"),
+                   url: "https://github.com/naseem123/chatbot_sdk_channel/releases/download/1.3.0/App.xcframework.zip",
+                   checksum: "bfc6927e39651ff35a92a5f6cf2bf4fdb43dbac44d265d69585fb5c91f78a589"),
              .binaryTarget(name: "Flutter",
-                           url: "https://github.com/naseem123/chatbot_sdk_channel/releases/download/1.2.9%2B6/Flutter.xcframework.zip",
-                           checksum: "3999423457cc4ee8786eb5eb57fa5134fa8f51726bdbffd8cb00ca9076f846b4"),
+                           url: "https://github.com/naseem123/chatbot_sdk_channel/releases/download/1.3.0/Flutter.xcframework.zip",
+                           checksum: "fbae5f60608e93f71a2e7ccca81140d0ed4b1a41ad56da7d2478d612dc762ca7"),
             .binaryTarget(name: "FlutterPluginRegistrant",
-                       url: "https://github.com/naseem123/chatbot_sdk_channel/releases/download/1.2.9%2B6/FlutterPluginRegistrant.xcframework.zip",
-                       checksum: "6b57af4c68b1a4c848db94c005bb202c71244a5c41b4e11052062aa8ac934f18"),
+                       url: "https://github.com/naseem123/chatbot_sdk_channel/releases/download/1.3.0/FlutterPluginRegistrant.xcframework.zip",
+                       checksum: "f2cc70d3e13d72be038a1395f9a1f62ff96ebbdddd3799ec2259a874cc9a0395"),
              .binaryTarget(name: "path_provider_foundation",
-                           url: "https://github.com/naseem123/chatbot_sdk_channel/releases/download/1.2.9%2B6/path_provider_foundation.xcframework.zip",
-                           checksum: "de8b4f3cafb7b9225612ea3baeab066a428b99547264a13237bd3f21116907a7"),
+                           url: "https://github.com/naseem123/chatbot_sdk_channel/releases/download/1.3.0/path_provider_foundation.xcframework.zip",
+                           checksum: "e964e8f224a12db6f61dbb7d0bee53b6c314f661761df1e46c91068d1959bccd"),
              .binaryTarget(name: "shared_preferences_foundation",
-                           url: "https://github.com/naseem123/chatbot_sdk_channel/releases/download/1.2.9%2B6/shared_preferences_foundation.xcframework.zip",
-                           checksum: "dae7789447e433cae8a934c9dca81111e64bdea082061fd074cf901aac940419"),
+                           url: "https://github.com/naseem123/chatbot_sdk_channel/releases/download/1.3.0/shared_preferences_foundation.xcframework.zip",
+                           checksum: "25f2c2687316b2856dafa477acad3696f562675e4bfe2cc2e3eb8046c0f3740f"),
 
              .binaryTarget(name: "sqflite",
-                           url: "https://github.com/naseem123/chatbot_sdk_channel/releases/download/1.2.9%2B6/sqflite.xcframework.zip",
-                           checksum: "82d596983ef695b0f2d69a99e0d35185453083bde89a6855ac1561fd3f1a517e"),
+                           url: "https://github.com/naseem123/chatbot_sdk_channel/releases/download/1.3.0/sqflite.xcframework.zip",
+                           checksum: "24d8e4ae845d2456d8626c29044da9c2661bfcaf20a0ae1e2e1ddb084c143d74"),
              .binaryTarget(name: "url_launcher_ios",
-                           url: "https://github.com/naseem123/chatbot_sdk_channel/releases/download/1.2.9%2B6/url_launcher_ios.xcframework.zip",
-                           checksum: "fd66be0f25572d40b45cbec4034227597cfe70a10f90edc4a2233e897b8842f3"),
+                           url: "https://github.com/naseem123/chatbot_sdk_channel/releases/download/1.3.0/url_launcher_ios.xcframework.zip",
+                           checksum: "e99a6178447d7aa7b8ac00dd502f2f0832b7946d793755c803f8d0e7654dd84c"),
          .binaryTarget(name: "webview_flutter_wkwebview",
-                       url: "https://github.com/naseem123/chatbot_sdk_channel/releases/download/1.2.9%2B6/webview_flutter_wkwebview.xcframework.zip",
-                       checksum: "8fce44c6366f59c09f0700ded338168b7ac4c2aeb7b125d922c70d8b45fd9271"),
+                       url: "https://github.com/naseem123/chatbot_sdk_channel/releases/download/1.3.0/webview_flutter_wkwebview.xcframework.zip",
+                       checksum: "47c1c2153d30586bca448a1ef899c865f9b7845c068430ecd07ea4d25d69d300"),
      ]
  )
