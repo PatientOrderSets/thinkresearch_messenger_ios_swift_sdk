0abd41faedb300f17818e27733887facd599e8bf
naseem-dev <naseem@prismcloudinc.com>
2024-06-19 21:37:35 +0530

    framework migration

--- Package.swift
+++ Package.swift
@@ -39,14 +39,13 @@ let package = Package(
              .binaryTarget(name: "shared_preferences_foundation",
                            url: "https://github.com/naseem-thinkresearch/thinkresearch_messenger_ios_swift_sdk/releases/download/1.0.6/shared_preferences_foundation.xcframework.zip",
                            checksum: "09afa6eeee34eb4640dbe38b57baaaf6338bcd8795dc0215fa9547b4997e0194"),
-
              .binaryTarget(name: "sqflite",
                            url: "https://github.com/naseem-thinkresearch/thinkresearch_messenger_ios_swift_sdk/releases/download/1.0.6/sqflite.xcframework.zip",
                            checksum: "65eab8635592efd222a7bb0e808fbac7040227f59e9bcaabd3b11293dca21619"),
              .binaryTarget(name: "url_launcher_ios",
                            url: "https://github.com/naseem-thinkresearch/thinkresearch_messenger_ios_swift_sdk/releases/download/1.0.6/url_launcher_ios.xcframework.zip",
                            checksum: "9affbf6b3f31907b65c0d590207ede927e3eaa00fba12aef8ebef004a60f05e0"),
-         .binaryTarget(name: "webview_flutter_wkwebview",
+            .binaryTarget(name: "webview_flutter_wkwebview",
                        url: "https://github.com/naseem-thinkresearch/thinkresearch_messenger_ios_swift_sdk/releases/download/1.0.6/webview_flutter_wkwebview.xcframework.zip",
                        checksum: "9d329be7ff8a6731c470fc75c25b7bfdc0338ce30e9fafba2960483449d797a1"),
      ]
